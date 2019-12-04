c0f0a24dc40ab411fd4a1bac976b1cc96bc42177
Jinwei <cwchang93@gmail.com>
2019-12-04 10:50:25 +0800

    async bug

--- src/components/dtm_rajw.tsx
+++ src/components/dtm_rajw.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./dtm_rajw.scss";
+const cx = require(`classnames`);
 
 interface Props {
   multiItem?: boolean; // 單選或多選
@@ -13,10 +14,24 @@ interface Props {
     Rajn?: any; //Rajn    null:不出現補字選單
   };
   chooseFirst?: boolean; //是否補第一筆 (PC才有)  Dtm 補左上角
+  inputCallBack?: any;
+  inputTxt?: string;
 }
 
 interface State {
   inputText: string;
+}
+
+interface I_dtmData {
+  line: { text: string; order: number }[];
+  country: {
+    text: string;
+    order: number;
+    content: {
+      text: string;
+      value: string;
+    }[];
+  }[];
 }
 
 const rajnArr = [
@@ -29,60 +44,83 @@ const rajnArr = [
   "阿拉伯聯合大公國(AE)__杜拜-DUBAI(DXB)"
 ];
 
-const ClickOutside = (ref: any, callback: any) => {
-  function handleClickOutside(event: any) {
-    if (ref.current && !ref.current.contains(event.target)) {
-      callback();
-    }
-  }
-
-  React.useEffect(() => {
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, [handleClickOutside]);
-};
-
 const DtmRajw: React.FC<Props> = (props: Props) => {
   //  const DtmData = props.data.Dtm && props.data.Dtm;
   const dtmData = props.data && props.data.Dtm;
   // const dtmData = props.data && props.data.Dtm
-  const [inputText, setInputText] = React.useState("");
-  const [showDtm, setShowDtm] = React.useState(false);
-  const [order, setOrder] = React.useState(-1);
+  const [order, setOrder] = React.useState(1);
   const [selectedId, setSelectedId] = React.useState("");
   const [selectedText, setSelectedText] = React.useState("");
-
-  const wrapperRef = React.useRef(null);
-  ClickOutside(wrapperRef, () => setShowDtm(false));
-
-  const onInputChange = (inputValue: string) => {
-    setInputText(inputValue);
-    console.log("inputValue", inputValue);
-    setShowDtm(false);
-  };
-
-  React.useEffect(() => {
-    if (inputText !== selectedText) {
-      setSelectedId("selectedId");
-    }
-  }, [inputText, selectedText]);
+  const [dotsArr, setDotsArr] = React.useState<Array<boolean>>([]);
+  //   const [dotsArr, setDotsArr] = React.useState<any>([]);
+  const [newDtmData, setNewDtmData] = React.useState<any>({});
 
   const handleSelected = (item: any, idx: number) => {
-    setInputText(item.text);
+    // setInputText(item.text);
     setSelectedText(item.text);
     setSelectedId(item.value);
+    console.log("idx", idx);
+  };
+
+  // didMount
+  React.useEffect(() => {
+    // 創一個新的data並加入Key值
+    const newDtmData = { ...dtmData };
+    newDtmData.line.map((ele: any) => {
+      ele.showDot = false;
+    });
+    setNewDtmData(newDtmData);
+  }, []);
+
+  React.useEffect(() => {
+    setSelectedText(props.inputTxt || "");
+    newDtmData.country.map((country: any) => {
+      // country的arr
+      country.content.map((city: any) => {
+        // country的content(arr)
+        if (props.inputTxt === city.text) {
+          const tmpDtmData = { ...newDtmData };
+          const dotOrder = country.order; // 要加點點的order
+          tmpDtmData.line.map((ele: any) => {
+            ele.showDot[dotOrder] = true;
+          });
+          setNewDtmData(tmpDtmData);
+        }
+      });
+    });
+  }, [props.inputTxt]);
+
+  const matchDots = (jsonData: I_dtmData) => {
+    const continentData = jsonData["line"];
+    const matchDotsArr = new Array(continentData.length).fill(false);
+    /**
+     * 比對資料並setDotsArr
+     * 1. 找出關鍵字對應的order值
+     * 2. 把order的值存下來
+     * 3. 把matchDotsArr的值continent加紅底=> 把該位置變true
+     */
+
+    setDotsArr(matchDotsArr);
+    console.log("matchDotsArr", matchDotsArr);
   };
 
   // DtmData &&  console.log('orgData.line',DtmData.line);
   const renderContinent = () => {
     console.log("continent");
-    return dtmData.line.map((ele: any) => {
+    return dtmData.line.map((ele: any, i: number) => {
       return (
         <li
-          onClick={() => setOrder(ele.order)}
-          className={ele.order === order ? "active" : ""}
+          key={i}
+          onClick={() => {
+            setOrder(ele.order);
+          }}
+          className={
+            cx({
+              // ["active"]: ele.order === order,
+              ["dot"]: dotsArr[i]
+            })
+            // ele.order === order ? "active" : ""
+          }
         >
           {ele.text}
         </li>
@@ -93,7 +131,7 @@ const DtmRajw: React.FC<Props> = (props: Props) => {
   const renderCity = () => {
     return dtmData.country.map((ele: any, i: number) => {
       return (
-        <div className="contentWrap" key={ele}>
+        <div className="contentWrap" key={i}>
           <div className="levelTwoWrap">
             {ele.order === order && <div className="levelTwo">{ele.text}</div>}
           </div>
@@ -102,12 +140,21 @@ const DtmRajw: React.FC<Props> = (props: Props) => {
               if (ele.order === order) {
                 return (
                   <div
+                    key={idx}
                     id={item.value}
                     className={
-                      item.value === selectedId ? "item selected" : "item"
+                      //   item.value === selectedId ? "item selected" : "item"  // 一開始用id
+                      item.text === selectedText || item.value === selectedId
+                        ? "item selected"
+                        : "item"
                     }
                     onClick={() => {
+                      //   matchDots(dtmData);
+                      const newDotsArr = new Array(dtmData.length).fill(false);
+                      newDotsArr[i] = true;
+                      setDotsArr(newDotsArr);
                       handleSelected(item, idx);
+                      props.inputCallBack(item.text); // 把選到的text傳回父層
                     }}
                   >
                     {item.text}
@@ -122,34 +169,11 @@ const DtmRajw: React.FC<Props> = (props: Props) => {
   };
 
   return (
-    <div className="dtm_rajw" ref={wrapperRef}>
-      {/* <input
-        placeholder="請輸入地點"
-        value={inputText}
-        onChange={e => {
-          onInputChange(e.target.value);
-        }}
-        onFocus={() => {
-          console.log("focus");
-          setShowDtm(true);
-        }}
-      /> */}
-      {/* {showDtm && ( */}
-      <div
-        className="dtm_rajw_dtmWrap"
-        tabIndex={-1}
-        // onBlur={()=> {
-        //     console.log('blur');
-        // setShowDtm(false)}}
-      >
-        <ul className="continentTab">
-          {dtmData.line && renderContinent()}
-          {/* {renderContinent()} */}
-        </ul>
+    <div className="dtm_rajw">
+      <div className="dtm_rajw_dtmWrap" tabIndex={-1}>
+        <ul className="continentTab">{dtmData.line && renderContinent()}</ul>
         {dtmData.country && renderCity()}
-        {/* {renderCity()} */}
       </div>
-      )}
     </div>
   );
 };
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -5,21 +5,41 @@ interface I_Pre_Props {
   data?: {
     Dtm: any;
   };
+  inputCallBack?: (text: any) => void;
 }
+
+const ClickOutside = (ref: any, callback: any) => {
+  function handleClickOutside(event: any) {
+    if (ref.current && !ref.current.contains(event.target)) {
+      console.log("clickOutside");
+      callback();
+    }
+  }
+
+  React.useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [handleClickOutside]);
+};
 
 const Preview: React.FC<I_Pre_Props> = props => {
   const [showDtm, setShowDtm] = React.useState(false);
   const [inputText, setInputText] = React.useState("");
 
+  const wrapperRef = React.useRef(null);
+  ClickOutside(wrapperRef, () => setShowDtm(false));
+
   const onInputChange = (inputValue: string) => {
     setInputText(inputValue);
-    console.log("inputValue", inputValue);
-    setShowDtm(false);
+    // console.log("inputValue", inputValue);
+    // setShowDtm(false);
   };
 
   return (
     <>
-      <div>
+      <div ref={wrapperRef}>
         <input
           placeholder="請輸入地點"
           value={inputText}
@@ -34,7 +54,15 @@ const Preview: React.FC<I_Pre_Props> = props => {
             setShowDtm(true);
           }}
         />
-        {showDtm && <DtmRajw data={props.data} />}
+        {showDtm && (
+          <DtmRajw
+            data={props.data}
+            inputTxt={inputText}
+            inputCallBack={(text: any) => {
+              setInputText(text);
+            }}
+          />
+        )}
       </div>
     </>
   );
